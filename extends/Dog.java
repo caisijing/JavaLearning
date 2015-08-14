f1c04821b7f1f7e6b9a5022e28d39e06b11697ee
Arthur.Tsai <Arthur_Tsai@163.com>
2015-08-14 15:29:35 +0800

    [extends][Add other functions in subclass]

--- extends/Dog.java
+++ extends/Dog.java
@@ -1,8 +1,14 @@
 public class Dog extends Animal{
+
+    static void jumping(){
+        System.out.println("Dog is jumping...");
+    }
+
     public static void main(String[] args){
         System.out.println("Dog extends Animal...");
         barking("Dog");
 	hungry("Dog");
 	sleeping("Dog");
+	jumping();
     }
 }
